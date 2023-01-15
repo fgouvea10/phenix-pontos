a546eaff58d4f5cf09960a6775fcafa3ff7a2bc9
Felipe Gouvea <felipegalvesouvea@gmail.com>
2023-01-15 18:55:07 -0300

    feat: fetch firebase plans

--- phenix-web/src/pages/index.tsx
+++ phenix-web/src/pages/index.tsx
@@ -1,5 +1,64 @@
-export default function Home() {
-  return (
-    <h1>hello world</h1>
-  )
+import { useRouter } from "next/router";
+import type { GetStaticProps } from "next";
+
+import { doc, getDoc } from "firebase/firestore";
+
+import { firestore } from "../lib/firebase";
+import { formatCurrency } from "../utils/format-currency";
+
+interface Plan {
+  name: string;
+  price: number;
 }
+
+interface Props {
+  plans: Plan[];
+}
+
+export default function Home({ plans }: Props) {
+  const router = useRouter();
+
+  function handleSelectPlan(plan: Plan) {
+    localStorage.setItem("phenix:redirect", JSON.stringify(plan));
+    router.push("/checkout");
+  }
+
+  return (
+    <div>
+      <h1>Planos</h1>
+      <ul>
+        {plans.map((plan, index) => (
+          <>
+            <li key={index}>
+              <strong>{plan.name}</strong>
+              <br />
+              <span>{plan.price}</span>
+              <br />
+              <button type="button" onClick={() => handleSelectPlan(plan)}>
+                Escolher
+              </button>
+            </li>
+            <br />
+            <br />
+          </>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const plansSnapshot = await getDoc(doc(firestore, "phenix-plans", "plans"));
+  const { plans = [] } = plansSnapshot.data() || {};
+
+  const plansWithFormatPrice = plans.map(({ name, price }: Plan) => ({
+    name: `Plano ${name}`,
+    price: formatCurrency(price) as string | number,
+  }));
+
+  return {
+    props: {
+      plans: plansWithFormatPrice,
+    },
+  };
+};
---
+++ phenix-web/src/utils/format-currency.ts
@@ -0,0 +1,3 @@
+export function formatCurrency(number: number) {
+  return number.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+}
