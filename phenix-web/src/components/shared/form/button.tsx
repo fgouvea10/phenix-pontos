a81c22544384aaa7e2dcfc7092a8992be4cfb658
Felipe Gouvea <felipegalvesouvea@gmail.com>
2023-01-18 22:26:11 -0300

    feat: add variant prop in button component

--- phenix-web/src/components/shared/form/button.tsx
+++ phenix-web/src/components/shared/form/button.tsx
@@ -1,11 +1,14 @@
 import { ButtonHTMLAttributes, ReactElement, ReactNode } from 'react';
 import { CircleNotch } from 'phosphor-react';
+import clsx from 'clsx';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   loading?: boolean;
   loadingMessage?: string;
   children: ReactNode;
   icon?: ReactElement;
+  className?: string;
+  variant?: 'primary' | 'secondary'
 };
 
 export function Button({
@@ -13,13 +16,34 @@ export function Button({
   loading,
   loadingMessage,
   icon,
+  className,
+  variant = 'primary',
   ...rest
 }: ButtonProps) {
+
+  if (variant === 'secondary') return (
+    <button
+      type="button"
+      disabled={loading}
+      className={clsx('w-full overflow-hidden p-4 text-stone-500 font-semibold uppercase flex gap-2 items-center justify-center text-sm hover:brightness-95 transition-all bg-stone-200', className)}
+      {...rest}
+    >
+      {!!loading ? (
+        <>
+          <CircleNotch className="animate-spin" />
+          {loadingMessage}
+        </>
+      ) : (
+        children
+      )}
+    </button>
+  )
+
   return (
     <button
       type="button"
       disabled={loading}
-      className="w-full p-4 text-white font-semibold uppercase flex gap-2 items-center justify-center text-sm hover:opacity-80 transition-opacity; bg-stone-900"
+      className={clsx('w-full overflow-hidden p-4 text-white font-semibold uppercase flex gap-2 items-center justify-center text-sm hover:opacity-90 transition-opacity bg-stone-900', className)}
       {...rest}
     >
       {!!loading ? (
